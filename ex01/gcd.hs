fd04db7b509c945e4d73a7385f9bb260fb9b0697
Max Mathys <threadox@gmail.com>
2019-02-25 10:23:20 +0100

    Fix PC Load Letter Error

--- ex01/gcd.hs
+++ ex01/gcd.hs
@@ -5,3 +5,9 @@ myGcd x y
   | x == y = x
   | y > x = myGcd y x
   | otherwise = myGcd (x-y) y
+
+gcdInt :: Int -> Int -> Int
+gcdInt x y
+  | x < 0 = gcdInt (-x) y
+  | y < 0 = gcdInt x (-y)
+  | otherwise = myGcd x y
