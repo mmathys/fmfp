4ffcbd65d5ff03a53d5f53d03264d6a97b27f3c6
Max Mathys <threadox@gmail.com>
2019-03-17 11:48:58 +0100

    sheet 4

--- Sheet4.hs
+++ Sheet4.hs
@@ -76,3 +76,22 @@ isHarshad n = n `mod` quersumme n == 0
 quersumme :: Int -> Int
 quersumme 0 = 0
 quersumme x = x `mod` 10 + quersumme (x `div` 10)
+
+-- assignment 4
+
+{-
+   In this exercise you are required to adapt the following function implementations of
+   f, g and h such that foldl, foldr, zip, zipWith, filter, curry, uncurry, etc. will
+   be used. That means, your task is to modify the lines 10-11, 15-19 and 23-28.
+-}
+
+f :: [[a]] -> [a]
+f = (foldr (++) []) . (map (\x -> reverse x)) 
+
+
+g :: Eq a => [a] -> [a] -> [a]
+g xs ys = map (\(x, y) -> x) (filter (\(x, y) -> x == y) (zip xs ys))
+
+
+h :: [Int] -> Int
+h = length . filter (\x -> even x)
